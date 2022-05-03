12a5dafb7e3588ada075e27d0e337ed86fc32072
marcociav <marco.bizzness@gmail.com>
2022-05-03 18:08:40 +0200

    Created test images folder in etl

--- model-creation/etl.py
+++ model-creation/etl.py
@@ -14,8 +14,10 @@ def get_image_names(img_directory, labels):
 if __name__ == '__main__':
     new_smile_dir = 'data/transformed/smile'
     new_non_smile_dir ='data/transformed/non_smile'
+    test_dir = 'data/transformed/test'
     Path(new_smile_dir).mkdir(parents=True, exist_ok=True)
     Path(new_non_smile_dir).mkdir(parents=True, exist_ok=True)
+    Path(test_dir).mkdir(parents=True, exist_ok=True)
 
     with open('data/labels/SMILE_list.txt') as f:
         smile_labels = f.read().split('\n')
@@ -28,6 +30,11 @@ if __name__ == '__main__':
     original_dir = 'data/images/faces'
     smile_image_names = get_image_names(original_dir, smile_labels)
     non_smile_image_names = get_image_names(original_dir, non_smile_labels)
+    
+    test_image_names = [
+        path.split('\\')[-1] for path in glob(f'{original_dir}/*.ppm') 
+        if path.split('\\')[-1] not in smile_labels + non_smile_labels
+    ]
 
     for name in smile_image_names:
         im = Image.open(f"{original_dir}/{name}")
@@ -35,3 +42,7 @@ if __name__ == '__main__':
     for name in non_smile_image_names:
         im = Image.open(f"{original_dir}/{name}")
         im.save(f"{new_non_smile_dir}/{name.replace('.ppm', '.jpg')}")
+
+    for name in test_image_names:
+        im = Image.open(f"{original_dir}/{name}")
+        im.save(f"{test_dir}/{name.replace('.ppm', '.jpg')}")
